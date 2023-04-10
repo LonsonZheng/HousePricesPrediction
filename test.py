846ad4bfdc45cea80155f291355cf9c1a6a982bb
YuanJH728 <108172002+YuanJH728@users.noreply.github.com>
2023-04-10 11:31:28 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from demo import Ui_MainWindow  # 导入 uiDemo4.py 中的 Ui_MainWindow 界面类
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):  # 继承 QMainWindow类和 Ui_MainWindow界面类
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)  # 初始化父类
+        self.setupUi(self)  # 继承 Ui_MainWindow 界面类
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)  # 在 QApplication 方法中使用，创建应用程序对象
+    myWin = MyMainWindow()  # 实例化 MyMainWindow 类，创建主窗口
+    myWin.show()  # 在桌面显示控件 myWin
+    sys.exit(app.exec_())  # 结束进程，退出程序
